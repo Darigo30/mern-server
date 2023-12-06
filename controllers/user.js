57e2f38bcbc33e5d877bdf16844586f2b146dcf8
darigo30 <darigomez30@gmail.com>
2023-12-05 21:37:13 -0300

    se obtiene usuario logeado y todos los usuarios

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,34 @@
+const User = require("../models/user")
+
+//Me devuelve los datos del usario que está logeado
+
 async function getMe(req, res) {
-    res.status(200).send({ msg: "OK"})
+    const { user_id } = req.user
+    const response = await  User.findById(user_id)
+    console.log(response)
+    if(!response) {
+        res.status(400).send({ msg: "No se ha encontrado usuario"})
+    } else {
+        res.status(200).send({ msg: "OK"})
+    }
+}
+//Obtener todos los usuarios de mi BD si están activos o no 
+
+async function getUsers(req, res) {
+    const { active } = req.query
+    let response = null
+
+    if(active === undefined) {
+        response = await  User.find()
+    } else {
+        response = await User.find({ active })
+    }
+
+    console.log(response)
+    res.status(200).send({msg: "Ok usuario Obtenido"})
 }
 
 module.exports = {
     getMe,
+    getUsers,
 }
--- router/user.js
+++ router/user.js
@@ -4,5 +4,6 @@ const md_auth = require("../middlewares/authenticated")
 
 const api = express.Router()
 api.get("/user/me", [md_auth.asureAuth], UserController.getMe)
+api.get("/users", [md_auth.asureAuth], UserController.getUsers)
 
 module.exports = api 
